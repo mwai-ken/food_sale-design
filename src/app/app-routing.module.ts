32a56d438e8e56e789843ccc3b9aab17f6a272f0
Kenneth Mwai <kenmwas150@gmail.com>
2023-10-05 13:24:43 -0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { ThirdPageComponent } from './third-page/third-page.component';
+import { SalesComponent } from './sales/sales.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+{path:"",redirectTo:'home',pathMatch:'full'},
+{path:'home', component:FirstPageComponent},
+
+  {path:'about', component:SecondPageComponent},
+  {path:'collection', component:ThirdPageComponent},
+  {path:'sales', component:SalesComponent},
+  {path:'contact', component:ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
