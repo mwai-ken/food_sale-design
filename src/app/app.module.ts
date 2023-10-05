616a576a61d4a3d922acf6b32d4ea71b7f4a89d5
Kenneth Mwai <kenmwas150@gmail.com>
2023-10-05 13:18:36 -0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { ThirdPageComponent } from './third-page/third-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { SalesComponent } from './sales/sales.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FirstPageComponent,
+    SecondPageComponent,
+    ThirdPageComponent,
+    FooterComponent,
+    SalesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
